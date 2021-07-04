dae274fb9316e074f2abed588f85cd37d999ca15
Dylan Chen <dylancfe15@gmail.com>
2021-07-04 01:23:36 -0400

    Initial UI

---
+++ Keyboard/Extensions/Extension+UIView.swift
@@ -0,0 +1,76 @@
+//
+//  Extension+UIView.swift
+//  Keyboard
+//
+//  Created by Difeng Chen on 7/4/21.
+//
+
+import UIKit
+
+extension UIView {
+
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+
+    @IBInspectable
+    var masksToBounds: Bool {
+        get {
+            return layer.masksToBounds
+        }
+        set {
+            layer.masksToBounds = newValue
+        }
+    }
+
+    @IBInspectable
+    var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+
+            layer.shadowRadius = newValue
+        }
+    }
+
+    @IBInspectable
+    var shadowOffset : CGSize{
+
+        get{
+            return layer.shadowOffset
+        }set{
+
+            layer.shadowOffset = newValue
+        }
+    }
+
+    @IBInspectable
+    var shadowColor : UIColor{
+        get{
+            return UIColor.init(cgColor: layer.shadowColor!)
+        }
+        set {
+            layer.shadowColor = newValue.cgColor
+        }
+    }
+
+    @IBInspectable
+    var shadowOpacity : Float {
+
+        get{
+            return layer.shadowOpacity
+        }
+        set {
+
+            layer.shadowOpacity = newValue
+
+        }
+    }
+}
